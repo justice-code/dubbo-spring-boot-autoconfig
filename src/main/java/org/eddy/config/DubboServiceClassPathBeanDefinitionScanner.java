d40b995a2aff781af8ab583770a1a4838a228b1b
Justice-love <eddyxu1213@126.com>
2017-12-22 17:56:39 +0800

    注册service bean

--- src/main/java/org/eddy/config/DubboServiceClassPathBeanDefinitionScanner.java
+++ src/main/java/org/eddy/config/DubboServiceClassPathBeanDefinitionScanner.java
@@ -1,22 +1,40 @@
 package org.eddy.config;
 
+import com.alibaba.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.util.ClassUtils;
 
 import java.util.Set;
 
 public class DubboServiceClassPathBeanDefinitionScanner extends ClassPathBeanDefinitionScanner {
+
     public DubboServiceClassPathBeanDefinitionScanner(BeanDefinitionRegistry registry) {
         super(registry);
     }
 
     public void registerFilter() {
-
+        addIncludeFilter(new AnnotationTypeFilter(Service.class));
     }
 
     @Override
     protected Set<BeanDefinitionHolder> doScan(String... basePackages) {
-        return super.doScan(basePackages);
+        Set<BeanDefinitionHolder> beanDefinitionHolderSet = super.doScan(basePackages);
+
+        beanDefinitionHolderSet.stream().forEach(beanDefinitionHolder -> {
+            registerServiceBean(beanDefinitionHolder, getRegistry());
+        });
+
+        return beanDefinitionHolderSet;
+    }
+
+    private void registerServiceBean(BeanDefinitionHolder beanDefinitionHolder, BeanDefinitionRegistry registry) {
+        BeanDefinition beanDefinition = beanDefinitionHolder.getBeanDefinition();
+
+        Class<?> beanClass = ClassUtils.resolveClassName(beanDefinition.getBeanClassName(), DubboServiceClassPathBeanDefinitionScanner.class.getClassLoader());
+        Service service = beanClass.getAnnotation(Service.class);
     }
 }
