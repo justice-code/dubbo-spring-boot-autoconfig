aea8e3ddb3d1073f3c1944dff99c5eb72e0b1142
Justice-love <eddyxu1213@126.com>
2017-12-22 17:39:37 +0800

    重写 doScan 方法

--- src/main/java/org/eddy/config/DubboServiceClassPathBeanDefinitionScanner.java
+++ src/main/java/org/eddy/config/DubboServiceClassPathBeanDefinitionScanner.java
@@ -1,7 +1,10 @@
 package org.eddy.config;
 
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+
+import java.util.Set;
 
 public class DubboServiceClassPathBeanDefinitionScanner extends ClassPathBeanDefinitionScanner {
     public DubboServiceClassPathBeanDefinitionScanner(BeanDefinitionRegistry registry) {
@@ -10,5 +13,10 @@ public class DubboServiceClassPathBeanDefinitionScanner extends ClassPathBeanDef
 
     public void registerFilter() {
 
+    }
+
+    @Override
+    protected Set<BeanDefinitionHolder> doScan(String... basePackages) {
+        return super.doScan(basePackages);
     }
 }
