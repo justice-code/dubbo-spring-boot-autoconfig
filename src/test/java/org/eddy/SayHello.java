da6879f4b827be44461eade4a6d80e85be81c953
Justice-love <eddyxu1213@126.com>
2017-12-25 14:43:19 +0800

    增加service测试

--- src/test/java/org/eddy/SayHello.java
+++ src/test/java/org/eddy/SayHello.java
@@ -2,7 +2,7 @@ package org.eddy;
 
 import com.alibaba.dubbo.config.annotation.Service;
 
-@Service
+@Service(parameters = {"today", "25", "yi", "yan"})
 public class SayHello implements Say{
     @Override
     public String say() {
