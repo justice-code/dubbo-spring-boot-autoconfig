616de17bd26b3ccf605933186e3f041c60ffd445
Justice-love <eddyxu1213@126.com>
2017-12-25 11:26:03 +0800

    build service bean definition

--- src/main/java/org/eddy/config/DubboServiceClassPathBeanDefinitionScanner.java
+++ src/main/java/org/eddy/config/DubboServiceClassPathBeanDefinitionScanner.java
@@ -1,14 +1,21 @@
 package org.eddy.config;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.dubbo.config.spring.ServiceBean;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.util.ClassUtils;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DubboServiceClassPathBeanDefinitionScanner extends ClassPathBeanDefinitionScanner {
 
@@ -36,5 +43,31 @@ public class DubboServiceClassPathBeanDefinitionScanner extends ClassPathBeanDef
 
         Class<?> beanClass = ClassUtils.resolveClassName(beanDefinition.getBeanClassName(), DubboServiceClassPathBeanDefinitionScanner.class.getClassLoader());
         Service service = beanClass.getAnnotation(Service.class);
+
+        BeanDefinitionBuilder serverBuilder = BeanDefinitionBuilder.rootBeanDefinition(ServiceBean.class)
+                .addConstructorArgValue(service)
+                .addPropertyReference("ref", beanDefinition.getBeanClassName())
+                .addPropertyValue("interfaceClass", service.interfaceClass() == null ? beanClass.getInterfaces()[0] : service.interfaceClass());
+
+        String[] registryConfigBeanNames = service.registry();
+        List<RuntimeBeanReference> registryRuntimeBeanReferences = beanReferences(registryConfigBeanNames);
+        if (! CollectionUtils.isEmpty(registryRuntimeBeanReferences)) {
+            serverBuilder.addPropertyValue("registries", registryRuntimeBeanReferences);
+        }
+
+        String[] protocols = service.protocol();
+        List<RuntimeBeanReference> protocolRuntimeBeanReferences = beanReferences(protocols);
+        if (! CollectionUtils.isEmpty(protocolRuntimeBeanReferences)) {
+            serverBuilder.addPropertyValue("protocols", protocolRuntimeBeanReferences);
+        }
+
+
+
+    }
+
+    private List<RuntimeBeanReference> beanReferences(String[] registryConfigBeanNames) {
+        return Arrays.stream(registryConfigBeanNames).map(name -> {
+            return new RuntimeBeanReference(name);
+        }).collect(Collectors.toList());
     }
 }
